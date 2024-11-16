2efc7e5a335a9f985b642222fd0a315a2c25c23a
IvanRodriguezPortas <ivan.rodriguez.portas@udc.es>
2024-11-16 21:24:20 +0100

    add a function to add QComboBox #37

--- src/buttons.py
+++ src/buttons.py
@@ -3,7 +3,7 @@ from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 
 class Button():
-    
+    # Function to add a QPushButton 
     def add_QPushButton(self,text:str,font_type:str,font_size:int,height:int,width:int,visibility:bool):
         button = QPushButton(text)
         button.setFont(QFont(font_type,font_size))
@@ -16,6 +16,7 @@ class Button():
         button.setVisible(visibility)
         return button
 
+    # Function to add a QRadioButton
     def add_QRadioButton(self,text:str,font_type:str,font_size:str,height:int,width:int,visibility:bool):
         button = QRadioButton(text)
         button.setFont(QFont(font_type,font_size))
@@ -27,3 +28,16 @@ class Button():
             button.setMinimumSize(width,height)
         button.setVisible(visibility)
         return button
+    
+    # Function to add a QComboBox
+    def add_QComboBox(self,items:list,height:int,width:int,visibility:bool):
+            button = QComboBox()
+            button.addItems(items)
+            if width == None:
+                button.setMinimumHeight(height)
+            if height == None:
+                button.setMinimumWidth(width)
+            else:
+                button.setMinimumSize(width,height)
+            button.setVisible(visibility)
+            return button
