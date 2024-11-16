a9cdfec6192ada8b81083911b76e8bc84868a632
IvanRodriguezPortas <ivan.rodriguez.portas@udc.es>
2024-11-16 21:19:51 +0100

    add a function to add QRadioButton #37

--- src/buttons.py
+++ src/buttons.py
@@ -5,13 +5,25 @@ from PyQt5.QtCore import Qt
 class Button():
     
     def add_QPushButton(self,text:str,font_type:str,font_size:int,height:int,width:int,visibility:bool):
-            button = QPushButton(text)
-            button.setFont(QFont(font_type,font_size))
-            if width == None:
-                button.setMinimumHeight(height)
-            if height == None:
-                button.setMinimumWidth(width)
-            else:
-                button.setMinimumSize(width,height)
-            button.setVisible(visibility)
-            return button
+        button = QPushButton(text)
+        button.setFont(QFont(font_type,font_size))
+        if width == None:
+            button.setMinimumHeight(height)
+        if height == None:
+            button.setMinimumWidth(width)
+        else:
+            button.setMinimumSize(width,height)
+        button.setVisible(visibility)
+        return button
+
+    def add_QRadioButton(self,text:str,font_type:str,font_size:str,height:int,width:int,visibility:bool):
+        button = QRadioButton(text)
+        button.setFont(QFont(font_type,font_size))
+        if width == None:
+            button.setMinimumHeight(height)
+        if height == None:
+            button.setMinimumWidth(width)
+        else:
+            button.setMinimumSize(width,height)
+        button.setVisible(visibility)
+        return button
