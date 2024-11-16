ce88225913aba71c92a079d9c11d8b52aca6ad06
IvanRodriguezPortas <ivan.rodriguez.portas@udc.es>
2024-11-16 21:17:18 +0100

    add a class button and a function to create a QPushButton #37

---
+++ src/buttons.py
@@ -0,0 +1,17 @@
+from PyQt5.QtWidgets import QPushButton, QButtonGroup, QRadioButton, QComboBox
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
+
+class Button():
+    
+    def add_QPushButton(self,text:str,font_type:str,font_size:int,height:int,width:int,visibility:bool):
+            button = QPushButton(text)
+            button.setFont(QFont(font_type,font_size))
+            if width == None:
+                button.setMinimumHeight(height)
+            if height == None:
+                button.setMinimumWidth(width)
+            else:
+                button.setMinimumSize(width,height)
+            button.setVisible(visibility)
+            return button
