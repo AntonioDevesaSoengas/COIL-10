c90b0eac20093ab17af8d9ded68af378e18e881f
AntonioDevesaSoengas <antonio.devesa.soengas@udc.es>
2024-11-29 16:53:07 +0100

    fix(model_saver): updated model saving process to include optional fields for flexibility and robustness ref #22

--- src/model_saver.py
+++ src/model_saver.py
@@ -1,9 +1,10 @@
 import joblib
-from PyQt5.QtWidgets import QFileDialog, QMessageBox, QWidget
+import joblib
+from PyQt5.QtWidgets import QFileDialog, QMessageBox
 
-class ModelSaver(QWidget):
-    def __init__(self, model, formula, r_squared, mse, input_columns, output_column, description):
-        super().__init__()
+
+class ModelSaver:
+    def __init__(self, model, formula, r_squared, mse, input_columns, output_column, description, graph=None):
         self.model = model
         self.formula = formula
         self.r_squared = r_squared
@@ -11,18 +12,19 @@ class ModelSaver(QWidget):
         self.input_columns = input_columns
         self.output_column = output_column
         self.description = description
+        self.graph = graph  
 
     def save_model_dialog(self):
-        """Abrir un cuadro de diálogo para seleccionar la ubicación de guardado."""
+        """Open a file dialog to save the model."""
         options = QFileDialog.Options()
         file_path, _ = QFileDialog.getSaveFileName(
-            self, "Guardar Modelo", "", "Archivos Joblib (*.joblib)", options=options
+            None, "Guardar Modelo", "", "Archivos Joblib (*.joblib)", options=options
         )
         if file_path:
             self.save_model(file_path)
 
     def save_model(self, file_path):
-        """Guarda el modelo en el archivo especificado."""
+        """Save the model to the specified file path."""
         try:
             model_data = {
                 'model': self.model,
@@ -31,9 +33,10 @@ class ModelSaver(QWidget):
                 'mse': self.mse,
                 'input_columns': self.input_columns,
                 'output_column': self.output_column,
-                'description': self.description
+                'description': self.description,
+                'graph': self.graph, 
             }
             joblib.dump(model_data, file_path)
-            QMessageBox.information(self, "Guardado exitoso", "El modelo se ha guardado correctamente.")
+            QMessageBox.information(None, "Éxito", "Modelo guardado exitosamente.")
         except Exception as e:
-            QMessageBox.critical(self, "Error", f"Error al guardar el modelo: {str(e)}")
+            QMessageBox.critical(None, "Error", f"No se pudo guardar el modelo: {str(e)}")
